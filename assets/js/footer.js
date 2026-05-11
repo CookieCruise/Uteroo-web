cd213d0da5855df4d2c1a32e4120ea357ae4881d
cookiecruise-admin <maricruzf95@gmail.com>
2026-05-11 18:04:42 +0200

    Cambios fixes

---
+++ assets/js/footer.js
@@ -0,0 +1,7 @@
+// Cargar el footer dinámicamente
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error cargando el footer:', error));
